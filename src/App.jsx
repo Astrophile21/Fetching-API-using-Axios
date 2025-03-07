ed5028df2259535512d1e9bbaaaf2ba7cabcaef5
Jerin Johny <johnyjerin93@gmail.com>
2025-03-07 17:09:29 +0530

    3RD commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState ,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
